067e48b62097681d927b2f1d7f5ac5c61feddf20
Amith Raravi <amith.raravi@gmail.com>
2022-11-01 17:08:53 +0100

    update mongoose and remove options

--- server/db.js
+++ server/db.js
@@ -9,11 +9,7 @@ let db = process.env.APP_DB;
 //   .then(() => console.log("MongoDB successfully connected"))
 //   .catch(err => console.log(err));
 
-mongoose.connect(db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-});
+mongoose.connect(db);
 mongoose.connection.on('error', console.error.bind(console, 'connection error'));
 mongoose.connection.once('open', function() {
   console.log('MongoDB successfully connected');
